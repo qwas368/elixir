cb5e2fae6b85c4f4aa9840134c8cff9e0dfce3aa
YJ HU <yj_hu@mayohr.com>
2018-03-01 10:54:59 +0800

    exercism hamming (update)

--- elixir_exercism/hamming/hamming.exs
+++ elixir_exercism/hamming/hamming.exs
@@ -8,18 +8,10 @@ defmodule Hamming do
   {:ok, 4}
   """
   @spec hamming_distance([char], [char]) :: non_neg_integer
+  def hamming_distance(strand1, strand2) when length(strand1) != length(strand2), do: {:error, "Lists must be the same length"}
   def hamming_distance(strand1, strand2) do
-    count_different_nucleotides(strand1, strand2)
+    Enum.zip(strand1, strand2)
+    |> Enum.count(&(elem(&1, 0) != elem(&1, 1)))
+    |> (&({:ok, &1})).()
   end
-
-  def count_different_nucleotides(strand1, strand2, count \\ 0)
-  def count_different_nucleotides([strand_h1 | strand_t1], [strand_h2 | strand_t2], count) do
-    cond do
-      strand_h1 == strand_h2 -> count_different_nucleotides(strand_t1, strand_t2, count)
-      true -> count_different_nucleotides(strand_t1, strand_t2, count + 1)
-    end
-  end
-  def count_different_nucleotides([], [], count), do: {:ok, count}
-  def count_different_nucleotides([], _strand2, _count), do: {:error, "Lists must be the same length"}
-  def count_different_nucleotides(_strand1, [], _count), do: {:error, "Lists must be the same length"}
 end
